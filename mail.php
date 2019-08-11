1e7cba268bf82e1dbd8f41d2dda9fca61399cf53
biljana-sretenoska <sretenoska69@gmail.com>
2019-08-11 18:09:43 +0200

    fix issues with links

--- mail.php
+++ mail.php
@@ -7,13 +7,11 @@ $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent,$mailheader) or die("Error!");
 
-
-
-
-
-
-
-
-
-
 ?>
+
+<html lang="en">
+  <head>
+</head>
+ <body>
+    <h3> Thanks for the massage</h3>
+</body>
